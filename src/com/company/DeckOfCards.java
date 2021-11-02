6de7592ced6a48d587a77a56473cd7015a2171f7
MeghnadShirole <shirolemeghnad10@gmail.com>
2021-11-02 08:43:18 +0530

    Added:Initialisaion of Deck of Cards Game

---
+++ src/com/company/DeckOfCards.java
@@ -0,0 +1,12 @@
+/*
+Program for deck of cards to be played with 4 players
+*/
+package com.company;
+
+//creating a public class
+public class DeckOfCards {
+    //setting ut the initial game and storing the suits and ranks for further usage
+    int[] deck =new int[52];
+    String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+    String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+}
