758c273c05657bc9bad1ade5538637db83a59cad
MeghnadShirole <shirolemeghnad10@gmail.com>
2021-11-14 21:06:04 +0530

    Added:Number of players to play the game

--- src/com/company/DecKOfCardsMain.java
+++ src/com/company/DecKOfCardsMain.java
@@ -3,19 +3,10 @@ package com.company;
 public class DecKOfCardsMain {
 
     public static void main(String[] args) {
-        /*
-         * printing the welcome message
-         */
-        System.out.println("\n*-*-*-* Welcome to Deck Of Cards *-*-*-*");
 
-        /*
-         * created object of the DeckOfCardsGame
-         */
         DeckOfCardsGame deckOfCardsGame = new DeckOfCardsGame();
-
-        /*
-         * Using Object reference calling the method of DeckOfCards Class
-         */
-        deckOfCardsGame.initializeCards();
+        deckOfCardsGame.welcome();
+        deckOfCardsGame.deckOfCards();
+        deckOfCardsGame.noOfPlayers();
     }
 }
